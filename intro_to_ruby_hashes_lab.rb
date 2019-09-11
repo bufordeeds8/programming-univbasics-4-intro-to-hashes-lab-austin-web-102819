bb75788557db0208cf5f103ed4ff93f1925b7bba
Buford Eeds <bufordeeds8@gmail.com>
2019-09-11 20:43:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,13 +21,13 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-
+my_hash_creator = {
+  key: "value"
+}
 end
 
 def read_from_hash(hash, key)
-read_from_hash = {
-  hash: key
-}
+
 end
 
 def update_counting_hash(hash, key)
